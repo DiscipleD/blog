3232f2a78f45df928cf50be3a5c26534cceea621
discipled <disciple.ding@gmail.com>
2016-04-27 18:27:57 +0800

    add new post private npm server

---
+++ client/app/posts/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jack on 16-4-26.
+ */
+
+import privateNpm from './private-npm';
+
+export default {
+	privateNpm
+};
---
+++ client/app/posts/private-npm/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jack on 16-4-27.
+ */
+
+import Post from '../../../util/post';
+
+import content from './private-npm.md';
+
+const post = new Post({
+	title: 'Private npm',
+	content
+});
+
+export default post;
