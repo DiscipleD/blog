dba063a80d7de3835732ace4059935af1da84443
discipled <disciple.ding@gmail.com>
2016-08-26 00:10:26 +0800

    refact(FetchService): refactor fetch service, combine common function into a service

--- client/common/service/FetchService.js
+++ client/common/service/FetchService.js
@@ -2,7 +2,13 @@
  * Created by jack on 16-8-24.
  */
 
-export const generatorUrl = (url, params) => typeof params === 'object' ? url + params : url + '?' + params;
+export const generatorUrl = (url = '', params = '') =>
+	params ? url + '?' + generatorQueryString(params) : url;
+
+export const generatorQueryString = params =>
+	typeof params === 'object'
+		? Object.keys(params).map(key => key + '=' + JSON.stringify(params[key])).join('&')
+		: params;
 
 export const status = response => {
 	if (response.status >= 200 && response.status < 300) {
@@ -13,3 +19,9 @@ export const status = response => {
 };
 
 export const json = response => response.json();
+
+const httpFetch = (url, options) => fetch(url, options)
+	.then(status)
+	.then(json);
+
+export default httpFetch;
--- client/common/service/PostService.js
+++ client/common/service/PostService.js
@@ -2,7 +2,7 @@
  * Created by jack on 16-4-27.
  */
 
-import * as FetchService from './FetchService';
+import httpFetch, * as FetchService from './FetchService';
 
 const GRAPHQL_URL_PREFIX = '/graphql';
 
@@ -11,22 +11,16 @@ export default class PostService {
 
 	getLatestPost() {
 		const GET_LATEST_POST_GRAPHQL = `query={blog{posts{id,name,createdDate,title,subtitle,headerImgName,tags{name,label}}}}`;
-		return fetch(FetchService.generatorUrl(GRAPHQL_URL_PREFIX, GET_LATEST_POST_GRAPHQL))
-			.then(FetchService.status)
-			.then(FetchService.json);
+		return httpFetch(FetchService.generatorUrl(GRAPHQL_URL_PREFIX, GET_LATEST_POST_GRAPHQL));
 	}
 
 	queryPostList() {
 		const QUERY_POST_LIST_GRAPHQL = `query={blog{posts{id,name,createdDate,title,subtitle,tags{name,label}}}}`;
-		return fetch(FetchService.generatorUrl(GRAPHQL_URL_PREFIX, QUERY_POST_LIST_GRAPHQL))
-			.then(FetchService.status)
-			.then(FetchService.json);
+		return httpFetch(FetchService.generatorUrl(GRAPHQL_URL_PREFIX, QUERY_POST_LIST_GRAPHQL));
 	}
 
 	getPostByName(postName) {
 		const GET_POST_BY_NAME_GRAPHQL = `query={blog{post(name: "${postName}"){id,name,createdDate,title,subtitle,headerImgName,content,tags{name,label}}}}`;
-		return fetch(FetchService.generatorUrl(GRAPHQL_URL_PREFIX, GET_POST_BY_NAME_GRAPHQL))
-			.then(FetchService.status)
-			.then(FetchService.json);
+		return httpFetch(FetchService.generatorUrl(GRAPHQL_URL_PREFIX, GET_POST_BY_NAME_GRAPHQL));
 	}
 }
